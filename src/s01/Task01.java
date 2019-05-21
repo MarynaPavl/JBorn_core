8fa5964c5b247a62dc41d5a5100999cf3f973c97
Marina Pavlytska <marinabushneva@gmail.com>
2019-05-21 16:56:33 +0300

    change city to my

--- src/s01/Task01.java
+++ src/s01/Task01.java
@@ -3,6 +3,6 @@ package s01;
 public class Task01 {
 
     public static void main(String[] args) {
-        System.out.println("Summer is coming soon");
+        System.out.println("Hello Dnepr");
     }
 }
