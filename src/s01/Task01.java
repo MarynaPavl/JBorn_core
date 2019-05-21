e4c9a6bf1fdbe6bf39239fffadedec6622013873
Marina Pavlytska <marinabushneva@gmail.com>
2019-05-21 16:43:24 +0300

    message change

--- src/s01/Task01.java
+++ src/s01/Task01.java
@@ -3,6 +3,6 @@ package s01;
 public class Task01 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Summer is coming soon");
     }
 }
