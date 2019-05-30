af76f6d7d1f38689205ec16ce75110596d917f0f
Marina Pavlytska <marinabushneva@gmail.com>
2019-05-30 21:53:16 +0300

    S02-T06

--- src/s02/Task6.java
+++ src/s02/Task6.java
@@ -3,26 +3,20 @@ package s02;
 import java.util.Scanner;
 
 public class Task6 {
-    static void squareCircle (double r, double p, double s, double n) {
-        s = p * Math.pow(r,2);
-        n = 2 * p *r;
-        System.out.println("Площадь круга равна:" + s);
-        System.out.println("Длина окружности равна:" + n);
+    static void squareCircle(double r) {
+        double square = Math.PI * Math.pow(r, 2);
+        double circle = 2 * Math.PI * r;
+        System.out.println("Площадь круга равна:" + square);
+        System.out.println("Длина окружности равна:" + circle);
     }
 
     public static void main(String[] args) {
         int y = requestNumber();
         double r;
         r = y;
-        double p = 3.14;
-        int x = 0;
-        double s;
-        s = x;
-        int z = 0;
-        double n;
-        n = z;
-        squareCircle(r,p,s,n);
+        squareCircle(r);
     }
+
     static int requestNumber() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число:");
