78bd13d648a84647ad45f761efa768dc29a02a80
Marina Pavlytska <marinabushneva@gmail.com>
2019-05-30 21:07:32 +0300

    S02-T07

--- src/s02/Task7.java
+++ src/s02/Task7.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class Task7 {
     public static void main(String[] args) {
-        int x = requestNumber();
-        double a = 0.0;
-        if (x>0){
-            double y = Math.pow(Math.sin(a),2)*x;
+        double x = requestNumber();
+        if (x > 0) {
+            double y = Math.pow(Math.sin(x), 2);
             System.out.println("Y равен:" + y);
-        }else{
-            double y = 1-2*Math.sin(a)* Math.pow(x,2);
+        } else {
+            double y = 1 - 2 * Math.sin(Math.pow(x, 2));
             System.out.println("Y равен:" + y);
         }
     }
+
     static int requestNumber() {
-        Scanner scanner = new Scanner(System.in
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число:");
         return scanner.nextInt();
     }
