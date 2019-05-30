2e5e71ced131dd7938bc10db8b3eab908eaf15d3
Marina Pavlytska <marinabushneva@gmail.com>
2019-05-30 19:19:33 +0300

    S02-T09

--- src/s02/Task9.java
+++ src/s02/Task9.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 
 public class Task9 {
     public static void main(String[] args) {
-        int N = requestNumber();
+        int n = requestNumber();
         int c = 10;
         int d, i = 0;
-        for(; N > 1; N =N / c) {
-            d = N % 10;
+        for (; n > 1; n = n / c) {
+            d = n % 10;
             i = i + d;
         }
-            System.out.println("Сумма цифр числа равна: "+i);
+        System.out.println("Сумма цифр числа равна: " + i);
 
     }
+
     static int requestNumber() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число:");
