9d825dc848c1158cfde1373dfd0b5096d2bafeae
Marina Pavlytska <marinabushneva@gmail.com>
2019-05-30 18:28:53 +0300

    S02-T10

--- src/s02/Task10.java
+++ src/s02/Task10.java
@@ -5,26 +5,27 @@ import java.util.Scanner;
 
 public class Task10 {
     public static void main(String[] args) {
-        int N = requestNumber();
-        int [] mass = new int [N];
-        int a=0;
-        int i=0;
-        int s=1;
-        int b=0;
+        int n = requestNumber();
+        int[] mass = new int[n];
+        int a = 0;
+        int i = 0;
+        int s = 1;
+        int b = 0;
 
-       for (i=0; i <=N-1;i++ ) {
-           mass[i] = b = requestNumber();
-       }
-           System.out.println(Arrays.toString(mass));
+        for (i = 0; i <= n - 1; i++) {
+            mass[i] = b = requestNumber();
+        }
+        System.out.println(Arrays.toString(mass));
 
-       for(i=0; i<N; i++){
-               if(i % 2 == 0){
-               a+=mass[i];
-           }else
-               s*=mass[i];
-       }
-        System.out.println("Сумма = "+a+", Произведение = "+s);
+        for (i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                a += mass[i];
+            } else
+                s *= mass[i];
+        }
+        System.out.println("Сумма = " + a + ", Произведение = " + s);
     }
+
     static int requestNumber() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число:");
