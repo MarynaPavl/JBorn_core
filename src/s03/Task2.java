e28e644269587c22d99a1d1a85c2ed44151e896d
Marina Pavlytska <marinabushneva@gmail.com>
2019-06-08 12:29:09 +0300

    S03-T02

---
+++ src/s03/Task2.java
@@ -0,0 +1,28 @@
+package s03;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        int n = requestNumber();
+        int [] array = new int[n];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(10);
+        }
+        System.out.println("Genetated massive:");
+        for (int i:array) {
+            System.out.print(array[i]);
+        }
+    }
+
+    static int requestNumber() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число:");
+        return scanner.nextInt();
+    }
+}
+
