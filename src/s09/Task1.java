31b8ae06d50c2592488f38b258eff3fac0555bbc
Marina Pavlytska <marinabushneva@gmail.com>
2020-04-24 18:01:59 +0300

    S09-T01

--- src/s09/Task1.java
+++ src/s09/Task1.java
@@ -4,33 +4,34 @@ import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
+import java.util.ArrayList;
+
 
 public class Task1 {
     public static void main(String[] args) {
         Path tmp = Path.of("C:\\Users\\admin\\IdeaProjects\\JBorn_Core_11_Pavlytska");
-        StringBuilder result = findFile(tmp, "Task1.java");
-        String[] strings = result.toString().split(" ");
-        System.out.println(Arrays.toString(strings));
+        ArrayList<Path> arrayList = findFile(tmp, "Task1.java");
+
+        System.out.println(arrayList.toString());
 
     }
 
-    public static StringBuilder findFile(Path path, String fileName) {
-        StringBuilder result = new StringBuilder();
+    public static ArrayList<Path> findFile(Path path, String fileName) {
+        ArrayList<Path> arrayList = new ArrayList<>();
         try (DirectoryStream<Path> dirstrm = Files.newDirectoryStream(path)) {
             for (Path newpath : dirstrm) {
                 if (Files.isDirectory(newpath)) {
-                    result.append(findFile(newpath, fileName));
+                    arrayList.addAll(findFile(newpath, fileName));
                 } else {
                     if (newpath.toString().contains(fileName)) {
-                        result.append(newpath.toAbsolutePath()).append("\n");
+                        arrayList.add(newpath.toAbsolutePath());
                     }
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return result;
+        return arrayList;
     }
 }
 
