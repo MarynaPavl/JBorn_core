d5757e3d6aaadece1b736284a844083085aa6596
Marina Pavlytska <marinabushneva@gmail.com>
2020-04-28 20:49:48 +0300

    S09-T03

---
+++ src/s09/Task3.java
@@ -0,0 +1,36 @@
+package s09;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class Task3 {
+    public static void main(String[] args) {
+        File file = new File("Test");
+        try {
+            InputStream inputStream = new FileInputStream(file);
+
+            String str = readAsString(inputStream, StandardCharsets.UTF_8);
+
+            System.out.println(str);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static String readAsString(InputStream inputStream, Charset charset) throws IOException {
+        String string;
+        int a;
+
+        Reader reader = new InputStreamReader(inputStream, charset);
+        StringWriter sw = new StringWriter();
+
+        while ((a = reader.read()) != -1) {
+            sw.write(a);
+        }
+        string = sw.toString();
+
+        return string;
+    }
+}
+
