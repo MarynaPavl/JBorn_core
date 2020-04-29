231e539be5a26986c69533602702f556c57e3c9e
Marina Pavlytska <marinabushneva@gmail.com>
2020-04-29 18:15:30 +0300

    S09-T03

--- src/s09/Task3.java
+++ src/s09/Task3.java
@@ -7,8 +7,7 @@ import java.nio.charset.StandardCharsets;
 public class Task3 {
     public static void main(String[] args) {
         File file = new File("Test");
-        try {
-            InputStream inputStream = new FileInputStream(file);
+        try (InputStream inputStream = new FileInputStream(file)) {
 
             String str = readAsString(inputStream, StandardCharsets.UTF_8);
 
@@ -16,6 +15,7 @@ public class Task3 {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 
     static String readAsString(InputStream inputStream, Charset charset) throws IOException {
