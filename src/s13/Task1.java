94308e9f48dc617429ac2e81bcbd269c02ff5855
Marina Pavlytska <marinabushneva@gmail.com>
2020-07-14 16:47:26 +0300

    S13-T01

--- src/s13/Task1.java
+++ src/s13/Task1.java
@@ -1,15 +1,11 @@
 package s13;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Task1 {
     public static void main(String[] args) throws IllegalAccessException {
         ObjectToSend object = new ObjectToSend(28, "Tom", new ArrayList<>());
-        List<ObjectToSend> number = new ArrayList<>();
-        number.add(object);
-
         Validator valid = new Validator();
-        System.out.println(valid.validate(object, number));
+        System.out.println(valid.validate(object));
     }
 }
--- src/s13/Validator.java
+++ src/s13/Validator.java
@@ -7,14 +7,20 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Validator {
-    boolean validate(Object obj, List<ObjectToSend> aClass) throws IllegalAccessException {
+    boolean validate(Object obj) throws IllegalAccessException {
         Field[] fields = obj.getClass().getDeclaredFields();
         for (Field field : fields) {
             field.setAccessible(true);
             if (field.isAnnotationPresent(Max.class) & field.isAnnotationPresent(Min.class)) {
                 Integer a = field.getAnnotation(Max.class).length();
-                Integer b = (Integer) field.get(obj);
                 Integer c = field.getAnnotation(Min.class).length();
+                if (field.getType() == String.class) {
+                    Integer b = Integer.valueOf((String) field.get(obj));
+                    if (b.compareTo(a) >= 0 | b.compareTo(c) <= 0) {
+                        return false;
+                    }
+                }
+                Integer b = (Integer) field.get(obj);
                 if (b.compareTo(a) >= 0 | b.compareTo(c) <= 0) {
 
                     return false;
@@ -35,6 +41,7 @@ public class Validator {
             if (field.isAnnotationPresent(NotEmpty.class)) {
                 NotEmpty notEmpty = field.getDeclaredAnnotation(NotEmpty.class);
                 if (notEmpty != null) {
+                    Object aClass = field.get(obj);
                     if ((aClass instanceof Collection) && ((Collection) aClass).isEmpty()) {
                         return false;
                     }
